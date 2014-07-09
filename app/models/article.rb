66e33a60d4a937bc237a02693ebe40a7f10edd3a
adam <akulisek@gmail.com>
2014-07-09 09:38:23 +0200

    Page loading test done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,18 @@
 class Article < ActiveRecord::Base
+  #belongs_to :author
+
+  def self.parse_from_zive(html)
+    doc = Nokogiri::HTML.parse(html)
+
+    doc.search('.articlesListItem .articlesListItemData').map do |div|
+      anchor = div.search('.articleTitle a').first
+      Article.create!(
+          zive_id: anchor[:href][/\/(\d+)\//, 1].to_i,
+          title: anchor.text,
+          perex: div.search('p').text
+      )
+    end
+
+  end
+
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
 
+  it 'loads html page' do
+    html = File.read(File.dirname(__FILE__) + '/fixtures/zive_page.html', encoding: 'utf-8')
+  end
+
+
   pending 'parses article' do
-    html = File.read(File.dirname(__FILE__) + '....')
+    html = File.read(File.dirname(__FILE__) + '/fixtures/zive_page.html', encoding: 'utf-8')
+    Article.parse_from_zive(html)
   end
 end
